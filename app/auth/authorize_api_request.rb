75066b451d9d4fd2bec7f267f417f9161e69dd5a
Jhonatan Martins <jhonatanbmartins@gmail.com>
2019-02-18 14:24:24 -0300

    fixes rubocop

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -5,7 +5,7 @@ class AuthorizeApiRequest
 
   def call
     {
-      user: user
+      user: user,
     }
   end
 
@@ -15,9 +15,7 @@ class AuthorizeApiRequest
 
   def user
     @user ||= decoded_auth_token.first if decoded_auth_token
-    unless @user['client'] == ENV['AUTH_CLIENT']
-      raise ExceptionHandler::InvalidToken, I18n.t('errors.request.invalid_token')
-    end
+    raise ExceptionHandler::InvalidToken, I18n.t('errors.request.invalid_token') unless @user['client'] == ENV['AUTH_CLIENT']
   rescue JWT::DecodeError
     raise ExceptionHandler::InvalidToken, I18n.t('errors.request.invalid_token')
   end
@@ -28,6 +26,7 @@ class AuthorizeApiRequest
 
   def http_auth_header
     return headers['Authorization'].split(' ').last if headers['Authorization'].present?
+
     raise ExceptionHandler::MissingToken, I18n.t('errors.request.missing_token')
   end
 end
